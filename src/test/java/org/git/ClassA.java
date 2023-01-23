d8272b93de92b2db6aa31407f4132531f024d2d9
sumithrajeeva <sumithrarms94@gmail.com>
2023-01-23 13:04:03 +0530

    firsttask1

---
+++ src/test/java/org/git/ClassA.java
@@ -0,0 +1,40 @@
+package org.git;
+
+public class ClassA {
+private void Tn() {
+		
+
+	}
+private void Kerala() {
+		
+
+	}
+
+private void Andhra() {
+	
+
+}
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
